f858f9ed61cf28128e749f28ce8363c6ba082444
Laureline Grand <laureline.grand@diiage.org>
2025-02-26 16:50:01 +0100

    Affichage des applications

--- Front/password-manager/src/app/components/application-list/application-list.component.ts
+++ Front/password-manager/src/app/components/application-list/application-list.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpProviderService } from '../../services/http-provider.service';
 
 @Component({
   selector: 'app-application-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './application-list.component.html',
   styleUrl: './application-list.component.scss'
 })
-export class ApplicationListComponent {
+export class ApplicationListComponent implements OnInit {
+  applications: any[] = [];
+  errorMessage: string | null = null;
 
+  constructor(private httpProvider: HttpProviderService) {}
+
+  ngOnInit() {
+    this.fetchApplications();
+  }
+
+  fetchApplications() {
+    this.httpProvider.getApplications().subscribe(
+      (data: any) => {
+        this.applications = Array.isArray(data) ? data : [];
+        this.errorMessage = null;
+      },
+      (error: any) => {
+        if (error.status === 404) {
+          this.errorMessage = 'Aucune application trouvée.';
+        } else {
+          this.errorMessage = 'Une erreur est survenue, veuillez réessayer.';
+        }
+        console.error(error);
+        this.applications = [];
+      }
+    )
+  }
 }
